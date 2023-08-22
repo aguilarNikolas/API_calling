6e78bdb42a5b9b8fb3c742d46ec8a7664a5f9941
Nikolas Nascimento Aguilar <nik.adcd@gmail.com>
2023-08-22 11:40:24 -0300

    working with a public API on JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // console.log('hello');
 
-fetch('https://reqres.in/api/users/') //it returns a Promise, we use '.then' to handle 
-    .then(res => res.json()) //it returns another Promise in jason, we use again '.then' to handle. Have to check if the response is OK.
-    .then(data => console.log(data))
+const url = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+
+let year;
+
+fetch(url) //it returns a Promise, we use '.then' to handle 
+    .then(response => response.json()) //it returns another Promise in jason, we use again '.then' to handle. Have to check if the response is OK.
+    .then(data => {
+        console.log(data);
+        // year = data.data[0].year;
+        // console.log(year);
+    });
+        
+
+// console.log(data.year);
 
 
 
